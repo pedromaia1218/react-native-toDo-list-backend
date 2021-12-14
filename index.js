20494439af6600817d7f9fcc24574ff7af4f073e
Pedro Henrique Maia <pedromaia1218@gmail.com>
2021-12-14 14:37:53 -0300

    User Signup and Signin

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
+
+app.get('/', (req, res) => {
+    res.status(200).send('Meu backend!')
+})
+
+app.listen(3000, () => {
+    console.log('Backend running....')
+})
