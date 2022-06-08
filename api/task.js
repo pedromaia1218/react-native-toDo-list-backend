bcc5c56036f78e5543a9e14840c3358ea32cf94b
Pedro Henrique Maia <pedromaia1218@gmail.com>
2022-06-08 19:06:51 -0300

    Task 50% Done

--- api/task.js
+++ api/task.js
@@ -1,1 +1,43 @@
 const moment = require('moment')
+
+module.exports = app => {
+    const getTaks = (req, res) => {
+        const date = req.query.date ? req.query.date
+            : moment().endOf('day').toDate()
+
+        app.db('tasks')
+            .where({ userId: req.user.id })
+            .where('estimateAt', '<=', date)
+            .orderby('esmtimateAt')
+            .then(tasks => res.json(tasks))
+            .catch(err => res.status(400).json(err))
+    }
+
+    const save = (req, res) => {
+        if (!req.body.desc.trim()) {
+            return res.status(400).send('Descrição é um campo obrigatório')
+        }
+
+        req.body.userId = req.user.id
+
+        app.db('tasks')
+            .insert(req.body)
+            .then(_ => res.status(204).send)
+            .catch(err => res.status(400).json(err))
+    }
+
+    const remove = (req, res) => {
+        app.db('tasks')
+            .where({id: req.params.id, userId: req.user.id })
+            .del()
+            .then(rowsDeleted => {
+                if(rows > 0){
+                    res.status(204).send()
+                }else{
+                    const msg = `Não foi encontrada task com id ${req.params.id}.`
+                    res.status(400).send(msg)
+                }
+            })
+            .catch(err => res.status(400).json(err))
+    }
+}
